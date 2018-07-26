f684a4a2607cb4ee0cc6f54d76a710338cc0a6ef
DESKTOP-ML92REE\rdr <mrsontu.tn@gmail.com>
2018-07-27 01:17:18 +0700

    Revert "Change project"
    This reverts commit c8c5fd014911a5cc27f09d7b584f6acc0e0c7efd.

--- TeDuShopData/Repositories/PostCategoryRepository.cs
+++ TeDuShopData/Repositories/PostCategoryRepository.cs
@@ -3,20 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using TeduShopData.Infrrastructure;
-using TeDuShopData.Infrrastructure;
-using TeduSopModel.Model;
 
 namespace TeDuShopData.Repositories
 {
-    public interface IPostCategoryRepository: IRepository<PostCategory>
+    class PostCategoryRepository
     {
-
-    }
-    public class PostCategoryRepository : RepositoryBase<PostCategory>, IPostCategoryRepository
-    {
-        public PostCategoryRepository(IDbFactory dbFactory) : base(dbFactory)
-        {
-        }
     }
 }
--- TeDuShopServie/PostCategoruService.cs
+++ TeDuShopServie/PostCategoruService.cs
@@ -3,62 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using TeDuShopData.Infrrastructure;
-using TeDuShopData.Repositories;
-using TeduSopModel.Model;
 
 namespace TeDuShopServie
 {
-    public interface IPostCategoryService
+    class PostCategoruServiec
     {
-        void Add(PostCategory postCategory);
-        void Update(PostCategory postCategory);
-        void Delete(int id);
-        IEnumerable<PostCategory> GetAll();
-        IEnumerable<PostCategory> GetAllByParent(int parentId);
-        PostCategory GetById(int Id);
-
-    }
-    public class PostCategoruService : IPostCategoryService
-    {
-        IPostCategoryRepository _postCategoryRepository;
-        IUnitOfWork _unitOfWork;
-        public PostCategoruService(IPostCategoryRepository postCategoryRepository,IUnitOfWork unitOfWork)
-        {
-            _postCategoryRepository = postCategoryRepository;
-            _unitOfWork = unitOfWork;
-        }
-
-        public void Add(PostCategory postCategory)
-        {
-
-            _postCategoryRepository.Add(postCategory);
-
-        }
-
-        public void Delete(int id)
-        {
-            _postCategoryRepository.Delete(id);
-        }
-
-        public IEnumerable<PostCategory> GetAll()
-        {
-            return _postCategoryRepository.GetAll();
-        }
-
-        public IEnumerable<PostCategory> GetAllByParent(int parentId)
-        {
-            return _postCategoryRepository.GetMulti(x => x.Status && x.ParentID == parentId);
-        }
-
-        public PostCategory GetById(int Id)
-        {
-            return _postCategoryRepository.GetSingleById(Id);
-        }
-
-        public void Update(PostCategory postCategory)
-        {
-            _postCategoryRepository.Update(postCategory);
-                }
     }
 }
